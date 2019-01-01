efb6e7c9775123ea5e3faf7fcf7f98b96ba62f70
Piotr Smialkowski <p.smialkowski@gmail.com>
2019-01-01 22:59:47 +0100

    Add server info

--- packages/ebus-agent/libs/cli.mjs
+++ packages/ebus-agent/libs/cli.mjs
@@ -8,20 +8,26 @@ import { setupExceptionHandler } from './utils/errors';
 
     
 commander.command('start')
-    .option('--ebus-host <host>', 'server host', 'localhost')
-    .option('--ebus-port <port>', 'server port', 8888)
-    .action(({ ebusPort, ebusHost }) => {
-        commands.start({ ebusPort, ebusHost });
+    .option('--host <host>', 'metric exporter host', 'localhost')
+    .option('--port <port>', 'metric exporter port', 9091)
+    .option('--ebus-host <host>', 'ebus service host', 'localhost')
+    .option('--ebus-port <port>', 'ebus service port', 8888)
+    
+    // .option('--wamp-url <url>', 'wamp server url', 'ws://localhost:8080/ws')
+    // .option('--wamp-realm <realm>', 'wamp server realm', 'shome')
+    .action(options => {
+        const { ebusPort, ebusHost, host, port } = options;
+        commands.start({
+            server: {
+                host,
+                port,
+            },
+            ebus: {
+                host: ebusHost,
+                port: ebusPort,
+            }
+        });
     });
-
-commander.command('info')
-    .option('--ebus-host <host>', 'server host', 'localhost')
-    .option('--ebus-port <port>', 'server port', 8888)
-    .action((options) => {
-        console.log(config);
-        commands.info(options);
-    });
-
 
 commander.version(pkg.version)
     .usage('[cmd]');
